bd65841bdb87ef7efc0786daccb9acfa7b2d0cd7
Nico Diaz <ndiaz@forcegrid.com>
2013-04-16 14:28:19 -0300

    Caso de uso: Envio de mails
    Modificacion: Se integra la biblioteca de PHPMailer para el envio de
    mails. Se agrega el caso de testeo el envio de un mail de prueba

--- config/config.php.example.php
+++ config/config.php.example.php
@@ -5,6 +5,14 @@ $config = array(
 	'port' => '3306', 
 	'user' => 'username', 
 	'password' => 'password', 
-	'dbname' => 'database'
+	'dbname' => 'database', 
+	
+	'email_host' => '74.125.140.108', 
+	'email_ssl' => 'tls', 
+	'email_port' => 587, 
+	'email_username' => 'user@gmail.com', 
+	'email_password' => 'password', 
+	'email_from_email' => 'user@gmail.com', 
+	'email_from_name' => 'No Reply Test'
 );
 
