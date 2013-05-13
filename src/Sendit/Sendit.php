e23ec6e4a6df3d6ea80daa762b2e1bd2fad1a38e
Nico Diaz <ndiaz@forcegrid.com>
2013-05-13 01:09:32 -0300

    Caso de uso: Licencias
    Modificacion: Se ajusta la licencia en LGPL-2.1

--- src/Sendit/Sendit.php
+++ src/Sendit/Sendit.php
@@ -9,11 +9,11 @@ use Sendit\Exception\Connection;
  *
  * @link      https://github.com/nicodiaz/sendit/ for the canonical source repository
  * @copyright Copyright (c) 2013 Nicolás Díaz País. (http://www.nicodp.com.ar)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @license   http://www.gnu.org/licenses/lgpl-2.1.html
  * 
  * @author nicodiaz
  * @package Sendit
- * @version 0.01
+ * @version 1.0.3
  */
 class Sendit
 {
